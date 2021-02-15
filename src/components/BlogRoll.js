3fba088bc79159f2e132a1ebef83d08518494c63
Kiril Stoilov <kiril.stoilov@gmail.com>
2021-02-15 21:39:52 +0200

    bug fix

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -47,7 +47,7 @@ class BlogRoll extends React.Component {
                   <br />
                   <br />
                   <Link className="button" to={post.fields.slug}>
-                    Виж още →
+                    Виж повече →
                   </Link>
                 </p>
               </article>
