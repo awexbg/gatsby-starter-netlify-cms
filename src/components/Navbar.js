fcc1ef6e23cfe84ddfe6354379f1a8511dfade4c
Kiril Stoilov <kiril.stoilov@gmail.com>
2021-02-15 19:05:26 +0200

    Translate navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,32 +61,17 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                За нас
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Продукти
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                Блог
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                Контакти
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
