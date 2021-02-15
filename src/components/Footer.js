79e21dc919edccb02fa8fcca99ef72028d9e1a6a
Kiril Stoilov <kiril.stoilov@gmail.com>
2021-02-15 20:22:43 +0200

    Change logo size in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,8 +14,8 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
+            alt="Essentials Trader"
+            style={{ width: '16em', height: '22em' }}
           />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
