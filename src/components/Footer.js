79fbd0240548d8c7a2ff2ff4be046255ceb4b349
Kiril Stoilov <kiril.stoilov@gmail.com>
2021-02-16 14:48:56 +0200

    Change faviicon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -76,7 +76,7 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="twitter" href="https://www.youtube.com/channel/UCvuIOtJMZFu0-wSdrp2nK4g/videos">
                   <img
                     className="fas fa-lg"
                     src={twitter}
